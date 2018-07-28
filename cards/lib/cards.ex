b1a226a4479538c92ae3f5a70d5b541e269c8a49
Albert Kliemke <albertkliemke@gmail.com>
2018-07-28 14:01:10 -0300

    Add list comprehension

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,13 +1,18 @@
 defmodule Cards do
   def create_deck do
-    ["As", "Dois", "Tres", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove"]
+   values = ["As", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Teen", "Jack", "Queen", "King"]
+   suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+   for suit <- suits, value <-values do
+    "#{value} of #{suit}"
+   end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
-  def contains?(deck, hand) do
-
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
