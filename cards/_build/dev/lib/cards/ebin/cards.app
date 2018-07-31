81d7811720226ca68a5c004f8543a281c8cc9922
Albert Kliemke <albertkliemke@gmail.com>
2018-07-30 21:35:17 -0300

    Add Testing, Doc Test and Documentarion.

--- cards/_build/dev/lib/cards/ebin/cards.app
+++ cards/_build/dev/lib/cards/ebin/cards.app
@@ -1,7 +1,6 @@
 {application,cards,
-             [{applications,[kernel,stdlib,elixir,logger]},
+             [{applications,[kernel,stdlib,elixir,logger,ex_doc]},
               {description,"cards"},
               {modules,['Elixir.Cards']},
               {registered,[]},
-              {vsn,"0.1.0"},
-              {extra_applications,[logger]}]}.
+              {vsn,"0.1.0"}]}.
--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards.
+  """
+
+  @doc """
+    Returns a list of string representing a deck of playing cards.
+  """
   def create_deck do
-   values = ["As", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Teen", "Jack", "Queen", "King"]
+   values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Teen", "Jack", "Queen", "King"]
    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 
    for suit <- suits, value <-values do
@@ -12,10 +19,33 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines wheter a deck contains a given card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remaindeer of the deck.
+    The `hand_size` argument indicates how many cards shoul
+    be in the hand.
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Hearts"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
